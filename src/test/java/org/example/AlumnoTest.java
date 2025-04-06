5695e49a1da6484dc5ea4377108a969038144e0e
BautiL <bleguia@frba.utn.edu.ar>
2025-04-05 22:02:53 -0300

    comentarios en tests

--- src/test/java/org/example/AlumnoTest.java
+++ src/test/java/org/example/AlumnoTest.java
@@ -13,6 +13,7 @@ class AlumnoTest {
 
     @Test
     public void alumnoNoPuedeAnotarseAF2PeroSiAPye() {
+        //crear materias
         Materia am1 = new Materia();
         Materia aga = new Materia();
         Materia lyed = new Materia();
@@ -20,10 +21,12 @@ class AlumnoTest {
         Materia f2 = new Materia();
         Materia pye = new Materia();
 
+        //crear listas de materias aprobadass y correlatividades
         List<Materia> materiasAprobadasAlumno = Arrays.asList(am1, aga, lyed);
         List<Materia> correlativasF2 = Arrays.asList(am1, f1);
         List<Materia> correlativasPye = Arrays.asList(am1, aga);
 
+        // setear correlatividades de materias a inscribirse
         f2.setCorrelativas(correlativasF2);
         pye.setCorrelativas(correlativasPye);
 
@@ -31,6 +34,7 @@ class AlumnoTest {
 
         alumno.setMateriasAprobadas(materiasAprobadasAlumno);
 
+        // comprobar que alumno puede anotarse a pye pero no a f2
         Assertions.assertFalse(alumno.puedeAnotarse(f2));
         Assertions.assertTrue(alumno.puedeAnotarse(pye));
     }
--- src/test/java/org/example/InscripcionTest.java
+++ src/test/java/org/example/InscripcionTest.java
@@ -12,27 +12,35 @@ class InscripcionTest {
 
     @Test
     public void intentosDeInscripcion() {
+        // crear materias
         Materia am1 = new Materia();
         Materia aga = new Materia();
         Materia lyed = new Materia();
         Materia f1 = new Materia();
         Materia f2 = new Materia();
         Materia pye = new Materia();
+
+        // instanciar incripciones
         Inscripcion inscripcion1 = new Inscripcion();
         Inscripcion inscripcion2 = new Inscripcion();
 
+        // crear listas de materias aprobadas, a inscribir y correlatividades
         List<Materia> materiasAprobadasAlumnoSinF1 = Arrays.asList(am1, aga, lyed);
         List<Materia> materiasAprobadasAlumnoConF1 = Arrays.asList(am1, aga, lyed, f1);
         List<Materia> correlativasF2 = Arrays.asList(am1, f1);
         List<Materia> correlativasPye = Arrays.asList(am1, aga);
         List<Materia> materiasInscripcion = Arrays.asList(f2, pye);
 
+        // setear correlatividades de materias a inscribir
         f2.setCorrelativas(correlativasF2);
         pye.setCorrelativas(correlativasPye);
 
+        // instanciar alumnos
         Alumno alumnoSinF1 = new Alumno();
         Alumno alumnoConF1 = new Alumno();
 
+
+        // setear materias aprobadas y materias a inscribir de cada alumno
         alumnoSinF1.setMateriasAprobadas(materiasAprobadasAlumnoSinF1);
         alumnoConF1.setMateriasAprobadas(materiasAprobadasAlumnoConF1);
 
@@ -42,6 +50,7 @@ class InscripcionTest {
         inscripcion2.setAlumno(alumnoConF1);
         inscripcion2.setMaterias(materiasInscripcion);
 
+        // testear que la inscripcion del alumno con las correlatividades necesarias pasa y viceversa
         Assertions.assertFalse(inscripcion1.aprobada());
         Assertions.assertTrue(inscripcion2.aprobada());
     }
